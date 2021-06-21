6ce9b621b81bfb6bf53c5f72ad9ecb49f7cb1077
SilenNaihin <silen.naihin@gmail.com>
2021-06-21 15:33:49 -0700

    readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # tabular-form
+
+* INSTRUCTIONS *
+Given a Component that will render _after_ page load, have that component
+  * fetch data from an API endpoint "/api/parents", used a random API found online from randomapi.com
+  * render results in tabular form with columns 
+
+Used Next.JS, TailwindCSS, react-select, and https://randomuser.me/ for the API. Built over a Sunday. Mobile friendly. 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,3 @@
-// * INSTRUCTIONS *
-// Given a Component that will render _after_ page load, have that component
-//   * fetch data from an API endpoint "/api/parents", used a random API found online from randomapi.com
-//   * render results in tabular form with columns 
-// uses Next.JS, TailwindCSS, react-select,  and https://randomuser.me/ for the API. Built over a Sunday. Mobile friendly. 
-
 import Table from "../components/table/table";
 import Header from "../components/header";
 import Hero from "../components/hero";
